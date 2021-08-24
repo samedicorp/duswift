88b60c3da5a4d59bf6e9e49ee066775d39906a79
Sam Deane <sam@elegantchaos.com>
2021-08-24 16:30:02 +0100

    Process whole log folder

--- Sources/duswift/LogProcessor.swift
+++ Sources/duswift/LogProcessor.swift
@@ -37,10 +37,12 @@ public class LogProcessor {
     var buyOrders: [Int:OrderList] = [:]
     var products: [String:Product] = [:]
     var schematics: [Int:CompactSchematic] = [:]
-    
+    var classes = LogParser.knownClasses
+    var addedClasses = false
+
     let printConstructKinds = false
     var constructKinds: Set<String> = []
-
+    
     let printObjectTypes = false
     var objectTypes: Set<String> = []
     
@@ -64,19 +66,21 @@ public class LogProcessor {
     }
     
     func loadProducts() {
+        print("Loading product index")
         let decoder = JSONDecoder()
         let url = dudeURL.appendingPathComponent("Products/combined.json")
         let decoded = try! decoder.decode([String:Product].self, from: Data(contentsOf: url))
         products = decoded
     }
-
+    
     func loadSchematics() {
+        print("Loading schematic index")
         let decoder = JSONDecoder()
         let url = dudeURL.appendingPathComponent("Schematics/compact.json")
         let decoded = try! decoder.decode([String:CompactSchematic].self, from: Data(contentsOf: url))
-//        schematics = decoded
+        //        schematics = decoded
     }
-
+    
     public func register(market: MarketInfo) {
         markets[market.id] = market
     }
@@ -106,7 +110,7 @@ public class LogProcessor {
             // skip if existing entry is newer
             return
         }
-
+        
         list.orders[order.orderId] = order
     }
     
@@ -122,53 +126,113 @@ public class LogProcessor {
     
     public func run() {
         print("Parsing file.")
-        let base = URL(fileURLWithPath: #file).deletingLastPathComponent().appendingPathComponent("../..")
-        let url = base.appendingPathComponent("/Extras/Logs/large.xml")
-        let parser = LogParser(url: url)
         loadProducts()
         loadSchematics()
         
+        let index = loadLogIndex()
         Task {
-            var classes = LogParser.knownClasses
-            var addedClasses = false
-            var count = 0
-
-            for await entry in parser.entryStream() {
-                count += 1
-                if !classes.contains(entry.class) {
-                    print("Found new class \(entry.class)")
-                    classes.insert(entry.class)
-                    addedClasses = true
-                }
-                
-                if let handler = handlers[entry.class] {
-                    handler.handle(entry, processor: self)
-                }
-                
-//                if entry.message.contains("ElementId"){
-//                    print(entry)
-//                }
-
-                if printObjectTypes {
-                    let message = entry.message
-                    let matches = objectPattern.matches(in: message, range: NSRange(location: 0, length: message.count))
-                    for match in matches {
-                        objectTypes.insert(String(message[match.range(at: 1)]))
-                    }
-                }
-                
+            for entry in index.values {
+                await processLog(entry)
             }
             
-            if addedClasses {
-                save(classes: classes)
-            }
-
+            saveLogIndex(index)
             finish()
         }
         
         dispatchMain()
     }
 
+    typealias LogIndex = [String:LogStatus]
+
+    class LogStatus: Codable {
+        let name: String
+        let url: URL
+        var lastLine: Int
+        
+        init(name: String, url: URL, lastLine: Int = 0) {
+            self.name = name
+            self.url = url
+            self.lastLine = lastLine
+        }
+    }
+
+    func loadLogIndex() -> LogIndex {
+        let decoder = JSONDecoder()
+        let base = URL(fileURLWithPath: #file).deletingLastPathComponent().appendingPathComponent("../..")
+        let logsFolder = ThrowingManager.default.folder(for: base.appendingPathComponent("/Extras/Logs/"))
+
+        let existingIndex: [String:LogStatus]
+        if let data = logsFolder.file("status.json").asData, let loaded = try? decoder.decode([String:LogStatus].self, from: data) {
+            existingIndex = loaded
+        } else {
+            existingIndex = [:]
+        }
+
+        var index: [String:LogStatus] = [:]
+        do {
+            try logsFolder.forEach { item in
+                if item.name.pathExtension == "xml" {
+                    let name = item.name.name
+                    let status = existingIndex[name] ?? LogStatus(name: name, url: item.url)
+                    index[name] = status
+                }
+            }
+        } catch {
+            print("Error iterating logs folder: \(error).")
+        }
+        
+        return index
+    }
+    
+    func saveLogIndex(_ index: LogIndex) {
+        let encoder = JSONEncoder()
+        let base = URL(fileURLWithPath: #file).deletingLastPathComponent().appendingPathComponent("../..")
+        let logsFolder = ThrowingManager.default.folder(for: base.appendingPathComponent("/Extras/Logs/"))
+        let logsFile = logsFolder.file("status.json")
+        do {
+            let encoded = try encoder.encode(index)
+            try encoded.write(to: logsFile.url)
+        } catch {
+            print("Failed to write log index. \(error)")
+        }
+    }
+    
+    func processLog(_ log: LogStatus) async {
+        print("Processing log \(log.name)")
+        if log.lastLine > 0 {
+            print("(skipping \(log.lastLine) lines.")
+        }
+        
+        var count = 0
+        let parser = LogParser(url: log.url)
+        for await entry in parser.entryStream(startingAtLine: log.lastLine) {
+            count += 1
+            if !classes.contains(entry.class) {
+                print("Found new class \(entry.class)")
+                classes.insert(entry.class)
+                addedClasses = true
+            }
+            
+            if let handler = handlers[entry.class] {
+                handler.handle(entry, processor: self)
+            }
+            
+            //                if entry.message.contains("ElementId"){
+            //                    print(entry)
+            //                }
+            
+            if printObjectTypes {
+                let message = entry.message
+                let matches = objectPattern.matches(in: message, range: NSRange(location: 0, length: message.count))
+                for match in matches {
+                    objectTypes.insert(String(message[match.range(at: 1)]))
+                }
+            }
+            
+            log.lastLine = entry.line
+        }
+    }
+    
     func exportConstructs() {
         print("Exporting constructs")
         let url = LogParser.baseURL.appendingPathComponent("../dudata/Constructs/")
@@ -214,25 +278,26 @@ public class LogProcessor {
         print("Sell Orders")
         for order in sellOrders {
             let product = order.key
-            print("Product \(product) \(String(format: "%x", product)):")
-//            print(product & 0x7FFFFFF)
-//            print(product & 0x3FFFFFF)
-//            print(product & 0x1FFFFFF)
-//            print(product & 0x0FFFFFF)
-//            let listings = order.value.orders.values.sorted(by: { $0.unitPrice.amount < $1.unitPrice.amount })
-//            for listing in listings {
-//                let market = markets[listing.marketId]?.name ?? ""
-//                print("\(listing.ownerName): \(-listing.buyQuantity) @ \(listing.unitPrice.amount), \(market)")
-//            }
+            print("\nProduct \(product):")
+            let listings = order.value.orders.values.sorted(by: { $0.unitPrice.amount < $1.unitPrice.amount })
+            for listing in listings {
+                if listing.ownerName != "marketbot" {
+                    let market = markets[listing.marketId]?.name ?? ""
+                    print("- \(listing.ownerName): \(-listing.buyQuantity) @ \(listing.unitPrice.amount), \(market)")
+                }
+            }
         }
     }
     
     func finish() -> Never {
-    
+        if addedClasses {
+            save(classes: classes)
+        }
+
         for handler in handlers.values {
             handler.finish(processor: self)
         }
-
+        
         if printConstructKinds {
             print("Found construct kinds")
             for kind in constructKinds {
@@ -252,7 +317,7 @@ public class LogProcessor {
         exportMarkets()
         exportRecipes()
         exportOrders()
-
+        
         print("Done.")
         exit(0)
     }
